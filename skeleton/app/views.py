987713c378284c790d34b991ca857d8151bb3409
Luke Gouldson <gouldsonluke@gmail.com>
2025-04-27 13:55:36 +0100

    Cleanup of unnecessary functions

--- skeleton/app/views.py
+++ skeleton/app/views.py
@@ -11,6 +11,7 @@ from app.processor import QG
 import csv
 import io
 
+
 @app.route("/")
 def home():
     return render_template('home.html', title="Home")
@@ -20,6 +21,7 @@ def home():
 @login_required
 def account():
     return render_template('account.html', title="Account")
+
 
 @app.route("/admin")
 @login_required
@@ -31,53 +33,17 @@ def admin():
     user_lst = db.session.scalars(q)
     return render_template('admin.html', title="Admin", user_lst=user_lst, form=form)
 
+
 @app.route("/staff")
 @login_required
 def staff():
     return render_template('staff.html', title="Staff")
 
+
 @app.route("/student")
 @login_required
 def student():
     return render_template('student.html', title="Student")
-
-@app.route('/delete_user', methods=['POST'])
-def delete_user():
-    form = ChooseForm()
-    if form.validate_on_submit():
-        u = db.session.get(User, int(form.choice.data))
-        q = db.select(User).where((User.role == "Admin") & (User.id != u.id))
-        first = db.session.scalars(q).first()
-        if not first:
-            flash("You can't delete your own account if there are no other admin users!", "danger")
-        elif u.id == current_user.id:
-            logout_user()
-            db.session.delete(u)
-            db.session.commit()
-            return redirect(url_for('home'))
-        else:
-            db.session.delete(u)
-            db.session.commit()
-    return redirect(url_for('admin'))
-
-@app.route('/toggle_user_role', methods=['POST'])
-def toggle_user_role():
-    form = ChooseForm()
-    if form.validate_on_submit():
-        u = db.session.get(User, int(form.choice.data))
-        q = db.select(User).where((User.role == "Admin") & (User.id != u.id))
-        first = db.session.scalars(q).first()
-        if not first:
-            flash("You can't drop your admin role if there are no other admin users!", "danger")
-        elif u.id == current_user.id:
-                logout_user()
-                u.role = "Student"
-                db.session.commit()
-                return redirect(url_for('home'))
-        else:
-            u.role = "Student" if u.role == "Admin" else "Admin"
-            db.session.commit()
-    return redirect(url_for('admin'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -97,30 +63,6 @@ def login():
             next_page = url_for('home')
         return redirect(next_page)
     return render_template('generic_form.html', title='Sign In', form=form)
-
-@app.route('/change_pw', methods=['GET', 'POST'])
-@fresh_login_required
-def change_pw():
-    form = ChangePasswordForm()
-    if form.validate_on_submit():
-        current_user.set_password(form.new_password.data)
-        db.session.commit()
-        flash('Password changed successfully', 'success')
-        return redirect(url_for('home'))
-    return render_template('generic_form.html', title='Change Password', form=form)
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = RegisterForm()
-    if form.validate_on_submit():
-        new_user = User(username=form.username.data, email=form.email.data)
-        new_user.set_password(form.password.data)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('home'))
-    return render_template('generic_form.html', title='Register', form=form)
 
 
 @app.route('/logout')
@@ -158,22 +100,22 @@ def question_form():
     return render_template('question_form.html', title = 'Question Form', form = form)
 
 
-# Error handlers
-# See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-
 # Error handler for 403 Forbidden
 @app.errorhandler(403)
 def error_403(error):
     return render_template('errors/403.html', title='Error'), 403
+
 
 # Handler for 404 Not Found
 @app.errorhandler(404)
 def error_404(error):
     return render_template('errors/404.html', title='Error'), 404
 
+
 @app.errorhandler(413)
 def error_413(error):
     return render_template('errors/413.html', title='Error'), 413
+
 
 # 500 Internal Server Error
 @app.errorhandler(500)
