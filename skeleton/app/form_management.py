6c40f7dbb33640189b88339f347f75adb18c3b78
Bridget Parris <bnparris@gmail.com>
2025-05-06 21:55:14 +0100

    added some docstrings

--- skeleton/app/form_management.py
+++ skeleton/app/form_management.py
@@ -4,6 +4,10 @@ from app.models import Student
 
 
 class FormManagement():
+    """
+    This class is used to release the forms, keep track of how long has elapsed since the forms were released, and
+    obtain a list of students who are behind on filling out their forms.
+    """
     release_date = None
     timeframe = 30
     released = False
@@ -12,18 +16,26 @@ class FormManagement():
             cls.instance = super(FormManagement, cls).__new__(cls)
         return cls.instance
     def set_release_date(self):
+        """
+        :return: the date and time that the release_forms button was clicked by the staff member
+        rtype: datetime
+        """
         self.release_date = datetime.now()
         self.released= True
     def calculate_seconds(self):
+        """
+        :return: The number of seconds since the form was released
+        """
         try:
             seconds = (datetime.now() - self.release_date).seconds
             return seconds
         except TypeError:
             return None
     def late_students(self):
-        #gets list of student objects for students who are behind on forms
-        #30 seconds used here represents 2 weeks
-
+        """
+        :return: list of student objects who are behind on filling out their forms
+        :rtype: int
+        """
         seconds = self.calculate_seconds()
         if seconds:
             q = db.select(Student).where(Student.forms_completed < seconds/self.timeframe -1)
